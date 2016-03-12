707e8bb5c8ca41f471a6fdeb3351d69bfa7a1a51
SivilTaram <qianlxc@126.com>
2016-03-12 14:47:56 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# BUAAOS-guide-book
-北航小操作系统实验指导书仓库
+# 北航小操作系统实验指导书
+
+[TOC]
+
+## 文件目录结构树
+|--- chapters (包含了lab1-lab6的主要内容tex文件)
+|--- codes (所有长段代码都在codes里有源文件)
+|--- fonts (四种adobe字体,在linux下安装后可以直接使用)
+|--- images (指导书中出现过的图片)
+|--- preface (前言，包含了老师寄语与编者寄语)
+|--- guide-book.tex (主tex文件，用于编译)
+|--- title-page.tex (封面tex文件)
+|--- Makefile 
+
+## 版本变更记录
+
+### 2016/3/12 
+指导书开源,版本号 v1.0.0
+### 2015/7/4 
+三位主要贡献者开始撰写指导书
+
+## 主要贡献者(按姓名拼音排序)
+
+- 何涛(@sighingnow)
+- 刘乾(@SivilTaram)
+- 王鹿鸣(@fzyz999)
---
+++ guide-book/codes/load_icode_mapper.c
@@ -0,0 +1,36 @@
+/* Overview:
+ *   This is a call back function for kernel's elf loader.
+ * Elf loader extracts each segment of the given binary image.
+ * Then the loader calls this function to map each segment
+ * at correct virtual address.
+ *
+ *   `bin_size` is the size of `bin`. `sgsize` is the
+ * segment size in memory.
+ *
+ * Pre-Condition:
+ *   va aligned 4KB and bin can't be NULL.
+ *
+ * Post-Condition:
+ *   return 0 on success, otherwise < 0.
+ */
+static int load_icode_mapper(u_long va, u_int32_t sgsize,
+         u_char *bin, u_int32_t bin_size, void *user_data)
+{
+	struct Env *env = (struct Env *)user_data;
+	struct Page *p = NULL;
+	u_long i;
+	int r;
+
+	/*Step 1: load all content of bin into memory. */
+	for (i = 0; i < bin_size; i += BY2PG) {
+	/* Hint: You should alloc a page and increase the reference count of it. */
+
+
+	}
+	/*Step 2: alloc pages to reach `sgsize` when `bin_size` < `sgsize`. */
+	while (i < sgsize) {
+
+
+	}
+	return 0;
+}
