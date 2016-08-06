693299b43c9da27e0db4ee732f1dcbaa59338304
TongyuYue <mycraftmw@163.com>
2016-08-07 04:05:22 +0800

    mkdir

--- guide-book/build.sh
+++ guide-book/build.sh
@@ -5,9 +5,7 @@ apt-get update && \
 apt-get install -y --no-install-recommends \
     texlive-full \
     python-pygments \
-    make \
-    wget \
-    unzip
+    make
 
 # install the fonts
 mkdir -p /usr/share/fonts/opentype/
@@ -15,13 +13,14 @@ cp /guide-book/fonts/AdobeFangsongStd-Regular.otf /usr/share/fonts/opentype/
 cp /guide-book/fonts/AdobeHeitiStd-Regular\ \(v5.010\).otf /usr/share/fonts/opentype/
 cp /guide-book/fonts/AdobeKaitiStd-Regular\ \(v5.010\).otf /usr/share/fonts/opentype/
 cp /guide-book/fonts/AdobeSongStd-Light\ \(v5.010\).otf /usr/share/fonts/opentype/
-mkfontscale
-mkfontdir
+sudo mkfontscale
+sudo mkfontdir
 fc-cache -fv
 fc-list :lang=zh
 
 # install the extra packages
-cp -rf /guide-book/extraPackage/*  /usr/share/texlive/texmf-dist/tex/latex/
+mkdir -p /usr/share/texlive/texmf-dist/tex/latex/
+cp -rf /guide-book/extraPackages/*  /usr/share/texlive/texmf-dist/tex/latex/
 
 # run make
 cd guide-book
