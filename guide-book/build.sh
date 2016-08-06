b73a8c64f7e39b351152b6f69fd39dbd1b1d4490
TongyuYue <mycraftmw@163.com>
2016-08-07 01:01:03 +0800

    ooops

--- guide-book/build.sh
+++ guide-book/build.sh
@@ -13,11 +13,31 @@ apt-get install -y --no-install-recommends \
     wget \
     xzdec
 
+# install the fonts
+mkdir -p /usr/share/fonts/opentype/
+cp /guide-book/fonts/AdobeFangsongStd-Regular.otf /usr/share/fonts/opentype/
+cp /guide-book/fonts/AdobeHeitiStd-Regular\ \(v5.010\).otf /usr/share/fonts/opentype/
+cp /guide-book/fonts/AdobeKaitiStd-Regular\ \(v5.010\).otf /usr/share/fonts/opentype/
+cp /guide-book/fonts/AdobeSongStd-Light\ \(v5.010\).otf /usr/share/fonts/opentype/
+mkfontscale
+mkfontdir
+fc-cache -fv
+fc-list :lang=zh
+
+# install the minted
+wget http://mirrors.ctan.org/macros/latex/contrib/minted.zip
+unzip minted.zip
+cd minted/
+make
+mv minted.sty /usr/share/texlive/texmf-dist/tex/latex/minted
+mv minted1.sty /usr/share/texlive/texmf-dist/tex/latex/minted
+cd ..
+apt-get install -y --no-install-recommends python-pygments
+
 # intsall latex packages
 tlmgr init-usertree
 tlmgr install ulem
 
-cd /guide-book
 # run make
-
+cd guide-book
 make
