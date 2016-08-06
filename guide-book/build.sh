c8d93e33986a2bdf180d754794295d5528081c2e
TongyuYue <mycraftmw@163.com>
2016-08-07 04:28:34 +0800

    copy the stys.

--- guide-book/build.sh
+++ guide-book/build.sh
@@ -13,14 +13,21 @@ cp /guide-book/fonts/AdobeFangsongStd-Regular.otf /usr/share/fonts/opentype/
 cp /guide-book/fonts/AdobeHeitiStd-Regular\ \(v5.010\).otf /usr/share/fonts/opentype/
 cp /guide-book/fonts/AdobeKaitiStd-Regular\ \(v5.010\).otf /usr/share/fonts/opentype/
 cp /guide-book/fonts/AdobeSongStd-Light\ \(v5.010\).otf /usr/share/fonts/opentype/
-sudo mkfontscale
-sudo mkfontdir
+mkfontscale
+mkfontdir
 fc-cache -fv
 fc-list :lang=zh
 
-# install the extra packages
-mkdir -p /usr/share/texlive/texmf-dist/tex/latex/
-cp -rf /guide-book/extraPackages/*  /usr/share/texlive/texmf-dist/tex/latex/
+# install minted.sty
+wget https://raw.githubusercontent.com/gpoore/minted/master/source/minted.sty
+wget https://raw.githubusercontent.com/gpoore/minted/master/source/minted1.sty
+mkdir -p /usr/share/texlive/texmf-dist/tex/latex/minted
+cp minted.sty minted1.sty /usr/share/texlive/texmf-dist/tex/latex/minted 
+
+#install fvextra.sty
+wget https://raw.githubusercontent.com/gpoore/fvextra/master/fvextra/fvextra.sty
+mkdir -p /usr/share/texlive/texmf-dist/tex/latex/fvextra
+cp fvextra.sty /usr/share/texlive/texmf-dist/tex/latex/fvextra
 
 # run make
 cd guide-book
